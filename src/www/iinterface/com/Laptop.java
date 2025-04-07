6afc3188a3d3030d514966f5c57731aca86441ec
technicalxakash <akashr2468@gmail.com>
2025-04-07 21:40:27 +0530

    Interface

--- src/www/iinterface/com/Laptop.java
+++ src/www/iinterface/com/Laptop.java
@@ -11,12 +11,12 @@ default void security() {
 	commonCode();
 	System.out.println("Laptop secuirty code");
 }
-
+//from java 1.8 we can implements the ,ethods in interface
 static void Printing() {
 	commonCode();
 	System.out.println("it can have the ability to print");
 }
-
+//from java 1.9 we can use private acces modifier to represent same code in all the methods
 private static void commonCode() {
 	System.out.println("CommonCode");
 }
