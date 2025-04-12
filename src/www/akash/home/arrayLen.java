17da883d22678f2b25f7572816f9a784adf2c681
technicalxakash <akashr2468@gmail.com>
2025-04-12 21:24:17 +0530

    string operations

---
+++ src/www/akash/home/arrayLen.java
@@ -0,0 +1,12 @@
+package www.akash.home;
+
+public class arrayLen {
+
+	public static void main(String[] args) {
+     int[] arr={1,2,3,4,5,56,7,8};
+	System.out.println("arr length is : "+arr.length);
+	System.out.println("arr minimum element : "+arr[0]);
+	System.out.println("arr maximum element : "+arr[arr.length-1]);
+
+}
+}
