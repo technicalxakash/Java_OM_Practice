1030f69b1f8ab6af4450dfab1a848dfe116aca40
technicalxakash <akashr2468@gmail.com>
2025-04-13 19:16:46 +0530

    exception

---
+++ src/www/exception/hand/Exception.java
@@ -0,0 +1,15 @@
+package www.exception.hand;
+
+public class Exception {
+
+	public static void main(String[] args) {
+		System.out.println("Program Started execution");
+		int fnumber=9;
+		int snumber=0;
+		int result;
+		result=fnumber/snumber;
+	  System.out.println("output is : "+result);
+	  System.out.println("Program excecution ended");
+	}
+
+}
