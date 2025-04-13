784c2c17315e66eacb6f02be23025f5d0e0f12dd
technicalxakash <akashr2468@gmail.com>
2025-04-13 19:52:54 +0530

    StackTrace

--- src/www/exception/hand/Exception.java
+++ src/www/exception/hand/Exception.java
@@ -1,3 +1,4 @@
+/*
 package www.exception.hand;
 
 public class Exception {
@@ -13,3 +14,57 @@ public class Exception {
 	}
 
 }
+*/
+
+/*Program Started execution
+Exception in thread "main" java.lang.ArithmeticException: / by zero
+at www.exception.hand.Exception.main(Exception.java:10)
+*/
+
+//Exception handling
+//we use  try,catch ,finally
+/*
+synatx= try {
+	which code will throws an exception that code is kept in try
+}
+*/
+
+
+//stacktrace
+/**
+ * Exception name=java.lang.ArithmeticException
+ * exception message =/ by zero
+ * which line number=
+ * methods info=
+ * 
+ * @param args
+ */
+
+package www.exception.hand;
+
+public class Exception {
+	
+	
+	
+	public static void test() {
+		
+		System.out.println("Program Started execution");
+		int fnumber=9;
+		int snumber=0;
+		int result;
+		result=fnumber/snumber;
+	  System.out.println("output is : "+result);
+	  System.out.println("Program excecution ended");
+}
+	
+	public static void test2() {
+		
+		test();
+	}
+	
+public static void main(String[] args) {
+	test2();
+	
+	
+}}
+
