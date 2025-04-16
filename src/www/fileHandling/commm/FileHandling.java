80dfebf38cc3881a76f3cf5f1a071ba6e377ce04
technicalxakash <akashr2468@gmail.com>
2025-04-16 18:05:59 +0530

    file operations

--- src/www/fileHandling/commm/FileHandling.java
+++ src/www/fileHandling/commm/FileHandling.java
@@ -9,13 +9,53 @@ public class FileHandling {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-      File f=new File("D:\\TestFiles\\resume.txt");
-      //System.out.println(f.createNewFile());
+      //File f=new File("D:\\TestFiles\\resume.txt");=Absoulte path
+      /*
+      System.out.println(f.createNewFile());
       
       
-      //doing deletion
+      //doing deletion =returns bollean
       System.out.println(f.delete());
       
+      
+      //combining both
+     System.out.println(f.createNewFile());
+      System.out.println(f.exists());
+      
+      
+      
+      //here it will create file or if file is present it will delete file 
+      if(f.exists()) {
+    	  f.delete();
+      }
+      else {
+    	  f.createNewFile();
+      }
+      
+      
+      
+      //hidden files =returns hidden
+      System.out.println(f.isHidden());
+     
+      
+      //readonly
+      System.out.println(f.canWrite());
+      
+      //removing readonly
+     f.setWritable(true);
+     System.out.println(f.canWrite());
+     
+      
+       //=relative path
+     File f=new File("./resume.txt");
+    
+     System.out.println(f.createNewFile());
+     System.out.println(f.delete());
+     System.out.println(f.exists());
+     */
+		
+		
+		
 	}
 
 }
